6416ef49294634d358ffc80a2be36cd6d14096ef
Mohammed Abdul Qahhar <abdulqahhar@Mohammeds-MacBook-Pro.local>
2022-10-27 12:16:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <Router-link to="/">
+      <h1>Movies<span>4</span>Life</h1>
+    </Router-link>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
+}
+
+body {
+  background-color: #000000;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #000000;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+    
+    span {
+      color: #ff0000;
+    }
+  }
+}
+
+</style>
